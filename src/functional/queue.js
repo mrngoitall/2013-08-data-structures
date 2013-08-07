921c07d317a2903ecae4d3b62d7256e7ba17de8d
Hack Reactor Students <github@hackreactor.com>
2013-08-07 14:54:06 -0700

    Added enqueue and dequeue

--- src/functional/queue.js
+++ src/functional/queue.js
@@ -2,15 +2,21 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size = 0;
+  var first = 0;
+  var last = 0;
 
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
+    storage[last] = value;
+    last++;
   };
 
   queue.dequeue = function(){
+    var result = storage[first];
+    first++;
+    return result;
   };
 
   queue.size = function(){
