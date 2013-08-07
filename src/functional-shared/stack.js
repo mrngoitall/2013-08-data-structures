65c29400890e87dd06c62bf229d9dfd02918f12c
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:21:58 -0700

    Copy and pasted framework from functional stack implementation

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,30 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0;
+
+  // Implement the methods below
+  var stack = {};
+  _.extend(stack, makeStack.stackMethods);
+  return stack;
+
 };
+
+makeStack.stackMethods = {};
+
+makeStack.stackMethods.push = function(value){
+  this.storage[this.size] = value;
+  this.size++;
+};
+
+makeStack.stackMethods.pop = function(){
+  var result = this.storage[this.size - 1];
+  this.size && this.size--;
+  return result;
+};
+
+makeStack.stackMethods.size = function(){
+  return this.size;
+};
+
