6805b3ce9b816f7dd7e3af4e02aa4c56c5934951
Hack Reactor Students <github@hackreactor.com>
2013-08-07 15:58:34 -0700

    Added pseudoclassical inheritance version of stack

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,21 @@
-var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function(){
+  this.storage = {};
+  this.stackSize = 0;
 };
+
+Stack.prototype.push = function(value){
+  this.storage[this.stackSize] = value;
+  this.stackSize++;
+};
+
+Stack.prototype.pop = function(){
+  var result = this.storage[this.stackSize - 1];
+  this.stackSize && this.stackSize--;
+  return result;
+};
+
+Stack.prototype.size = function(){
+  return this.stackSize;
+};
+
+var stack = new Stack();
